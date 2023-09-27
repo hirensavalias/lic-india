fea22a2b381acdef189d064998cc5ee54de8a57a
hirensavalias <hiren.savalias@gmail.com>
2023-09-27 22:55:29 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex justify-center items-center h-full">
-         //*<Image
-          src="/logo.png"
-          alt="LIC Logo"
-          width={200}
-          height={200}
-          priority
-          unoptimized
-        />*//
+         hello
     </main>
   )
 }
